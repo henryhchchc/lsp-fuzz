1ac127f8659f3070fb1444dc3f57c0df05ec46cd
Henry Chu <henrytech@outlook.com>
2025-04-11 14:52:31 +0800

    Move range selectors into separate module

---
+++ crates/lsp-fuzz/src/lsp/generation/doc_range/mod.rs
@@ -0,0 +1,73 @@
+use std::{marker::PhantomData, result::Result};
+
+use derive_new::new as New;
+use libafl::state::HasRand;
+use lsp_types::{Range, TextDocumentIdentifier};
+
+use super::{GenerationError, LspParamsGenerator};
+use crate::{
+    lsp::HasPredefinedGenerators,
+    lsp_input::LspInput,
+    text_document::{
+        TextDocument,
+        mutations::{TextDocumentSelector, text_document_selectors::RandomDoc},
+    },
+};
+
+mod range_selectors;
+
+#[derive(Debug)]
+pub struct Selection(pub TextDocumentIdentifier, pub Range);
+
+#[derive(Debug, New)]
+pub struct RangeInDocGenerator<State, D> {
+    range_selector: fn(&mut State, &TextDocument) -> Range,
+    _phantom: PhantomData<D>,
+}
+
+impl<State, D> Clone for RangeInDocGenerator<State, D> {
+    fn clone(&self) -> Self {
+        Self::new(self.range_selector)
+    }
+}
+
+impl<State, D> LspParamsGenerator<State> for RangeInDocGenerator<State, D>
+where
+    D: TextDocumentSelector<State>,
+{
+    type Output = Selection;
+
+    fn generate(
+        &self,
+        state: &mut State,
+        input: &LspInput,
+    ) -> Result<Self::Output, GenerationError> {
+        let (uri, doc) =
+            D::select_document(state, input).ok_or(GenerationError::NothingGenerated)?;
+        let range = (self.range_selector)(state, doc);
+        let doc = TextDocumentIdentifier { uri };
+        Ok(Selection(doc, range))
+    }
+}
+
+impl<State> HasPredefinedGenerators<State> for Selection
+where
+    State: HasRand,
+{
+    type Generator = RangeInDocGenerator<State, RandomDoc>;
+
+    fn generators() -> impl IntoIterator<Item = Self::Generator>
+    where
+        State: HasRand + 'static,
+    {
+        [
+            range_selectors::whole_range,
+            range_selectors::random_range,
+            range_selectors::random_range,
+            range_selectors::after_range,
+            range_selectors::inverted_range,
+            range_selectors::random_subtree,
+        ]
+        .map(RangeInDocGenerator::new)
+    }
+}
---
+++ crates/lsp-fuzz/src/lsp/generation/doc_range/range_selectors.rs
@@ -0,0 +1,78 @@
+
+use libafl::state::HasRand;
+use libafl_bolts::rands::Rand;
+use lsp_types::{Position, Range};
+
+use crate::text_document::{
+    GrammarBasedMutation, TextDocument, grammar::tree_sitter::TreeIter,
+};
+
+fn lsp_whole_range(doc: &TextDocument) -> Range {
+    let start = lsp_types::Position::default();
+    let end = doc
+        .lines()
+        .enumerate()
+        .last()
+        .map(|(line_idx, line)| lsp_types::Position::new(line_idx as _, line.len() as _))
+        .unwrap_or_default();
+    lsp_types::Range::new(start, end)
+}
+
+pub(super) fn whole_range<State>(_: &mut State, doc: &TextDocument) -> Range {
+    lsp_whole_range(doc)
+}
+
+pub(super) fn random_range<State: HasRand>(state: &mut State, doc: &TextDocument) -> Range {
+    let rand = state.rand_mut();
+    let lines: Vec<_> = doc.lines().collect();
+    let start_line_idx = rand.below_or_zero(lines.len());
+    let start_line = lines[start_line_idx];
+    let end_line_idx = rand.between(start_line_idx, lines.len() - 1);
+    let end_line = lines[end_line_idx];
+    let start = Position {
+        line: start_line_idx as u32,
+        character: rand.below_or_zero(start_line.len()) as u32,
+    };
+    let end = Position {
+        line: end_line_idx as u32,
+        character: rand.below_or_zero(end_line.len()) as u32,
+    };
+    Range { start, end }
+}
+
+pub(super) fn random_subtree<State: HasRand>(state: &mut State, doc: &TextDocument) -> Range {
+    let tree_iter = doc.parse_tree().iter();
+    if let Some(node) = state.rand_mut().choose(tree_iter) {
+        let start = node.start_position();
+        let start = Position {
+            line: start.row as u32,
+            character: start.column as u32,
+        };
+        let end = node.end_position();
+        let end = Position {
+            line: end.row as u32,
+            character: end.column as u32,
+        };
+        Range { start, end }
+    } else {
+        lsp_whole_range(doc)
+    }
+}
+
+pub(super) fn after_range<State>(_: &mut State, doc: &TextDocument) -> Range {
+    let Range { end, .. } = lsp_whole_range(doc);
+    let start = end;
+    let end = Position {
+        line: 65536,
+        character: end.character,
+    };
+    Range { start, end }
+}
+
+pub(super) fn inverted_range<State>(_: &mut State, doc: &TextDocument) -> Range {
+    let Range { start, end } = lsp_whole_range(doc);
+    Range {
+        start: end,
+        end: start,
+    }
+}
