992b51b5ada52ac086397a81096c701f83cfb5e1
Henry Chu <henrytech@outlook.com>
2025-05-10 23:46:48 +0800

    Reduce corpus cache size from 16384 to 1024

--- crates/lsp-fuzz-cli/src/fuzzing/common.rs
+++ crates/lsp-fuzz-cli/src/fuzzing/common.rs
@@ -70,7 +70,7 @@ pub fn create_corpus<I>(
 where
     I: Input,
 {
-    const CACHE_LEN: usize = 16384;
+    const CACHE_LEN: usize = 1024;
     let corpus = CachedOnDiskCorpus::with_meta_format(
         corpus_path,
         CACHE_LEN,
