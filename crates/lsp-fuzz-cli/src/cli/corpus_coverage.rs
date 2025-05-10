58784c9a11215cdcbf11bf5105983e4c40f9f7a7
Henry Chu <henrytech@outlook.com>
2025-05-10 17:48:09 +0800

    Add corpus type support to corpus coverage command

--- crates/lsp-fuzz-cli/src/cli/corpus_coverage.rs
+++ crates/lsp-fuzz-cli/src/cli/corpus_coverage.rs
@@ -1,7 +1,13 @@
-use std::{fs::File, path::PathBuf};
+use std::{
+    fs::File,
+    io::BufReader,
+    path::{Path, PathBuf},
+};
 
 use anyhow::Context;
-use lsp_fuzz::lsp_input::LspInput;
+use libafl::inputs::{BytesInput, Input, NautilusInput};
+use libafl_bolts::serdeany::SerdeAnyMap;
+use lsp_fuzz::{baseline::BaselineInput, corpus::GeneratedStats, lsp_input::LspInput};
 
 use crate::{cli::GlobalOptions, fuzzing::FuzzerStateDir};
 
@@ -23,6 +29,9 @@ pub struct CorpusCoverage {
     /// The path to the output file.
     #[clap(long, short)]
     output_file: PathBuf,
+
+    #[clap(long, value_enum)]
+    corpus_type: CorpusType,
 }
 
 impl CorpusCoverage {
@@ -44,8 +53,68 @@ impl CorpusCoverage {
                 it.file_name()
                     .into_string()
                     .expect("File name should be valid UTF-8")
-            });
+            })
+            .map(|it| self.corpus_type.load_input(&self.state.solution_dir(), it));
 
         Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct CoverageInput {
+    id: String,
+    input: CoverageInputRepr,
+    stats: GeneratedStats,
+}
+
+#[derive(Debug)]
+pub enum CoverageInputRepr {
+    LspFuzz(LspInput),
+    BaselineBin(BaselineInput<BytesInput>),
+    BaselineGram(BaselineInput<NautilusInput>),
+}
+
+#[derive(Debug, Clone, Copy, clap::ValueEnum)]
+pub enum CorpusType {
+    LspFuzz,
+    BaselineBin,
+    BaselineGram,
+}
+
+impl CorpusType {
+    fn load_input(
+        &self,
+        corpus_dir: &Path,
+        input_file_name: String,
+    ) -> anyhow::Result<CoverageInput> {
+        let input_path = corpus_dir.join(&input_file_name);
+        let input = match self {
+            CorpusType::LspFuzz => {
+                let input = LspInput::from_file(input_path).context("Loading LSP input")?;
+                CoverageInputRepr::LspFuzz(input)
+            }
+            CorpusType::BaselineBin => {
+                let input = BaselineInput::from_file(input_path)
+                    .context("Loading baseline binary input")?;
+                CoverageInputRepr::BaselineBin(input)
+            }
+            CorpusType::BaselineGram => {
+                let input = BaselineInput::from_file(input_path)
+                    .context("Loading baseline grammar input")?;
+                CoverageInputRepr::BaselineGram(input)
+            }
+        };
+        let metadata_reader = File::open(corpus_dir.join(format!(".{input_file_name}.metadata")))
+            .context("Opening metadata file")?;
+        let metadata_reader = BufReader::new(metadata_reader);
+        let mut metadata_map: SerdeAnyMap =
+            serde_json::from_reader(metadata_reader).context("Deserializing metadata")?;
+        let stats: GeneratedStats = *metadata_map.remove().context("Getting GeneratedStats")?;
+
+        Ok(CoverageInput {
+            id: input_file_name,
+            input,
+            stats,
+        })
     }
 }
