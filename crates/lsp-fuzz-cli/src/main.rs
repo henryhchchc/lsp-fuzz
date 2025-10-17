4c2f3cb9049a267b7250b246ad350e5c271f40ed
Henry Chu <henrytech@outlook.com>
2025-10-17 09:13:33 +0800

    Remove rustversion dependency and nightly warning

--- crates/lsp-fuzz-cli/src/main.rs
+++ crates/lsp-fuzz-cli/src/main.rs
@@ -9,13 +9,6 @@ use clap::Parser;
 pub const PROGRAM_NAME: &str = "LSP-Fuzz";
 
 fn main() -> anyhow::Result<()> {
-    if rustversion::cfg!(not(nightly)) {
-        eprintln!(
-            "WARNING: {PROGRAM_NAME} was not compiled with a nightly compiler. \
-                      Nightly-only optimizations are not available. \
-                      Performance will be lower than expected."
-        );
-    }
     let cli = cli::Cli::parse();
     cli.run().context("Running CLI")?;
     Ok(())
