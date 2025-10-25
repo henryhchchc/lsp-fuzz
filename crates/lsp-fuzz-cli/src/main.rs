101ea7e4d9241ca1d21fa13b2ca346c868486ab9
Henry Chu <henrytech@outlook.com>
2025-10-25 18:18:53 +0800

    Add debug build warning for performance

--- crates/lsp-fuzz-cli/src/main.rs
+++ crates/lsp-fuzz-cli/src/main.rs
@@ -9,6 +9,12 @@ use clap::Parser;
 pub const PROGRAM_NAME: &str = "LSP-Fuzz";
 
 fn main() -> anyhow::Result<()> {
+    if cfg!(debug_assertions) {
+        eprintln!(
+            "This is a debug build of {PROGRAM_NAME}.\nPlease use a release build for better performance."
+        )
+    }
+
     let cli = cli::Cli::parse();
     cli.run().context("Running CLI")?;
     Ok(())
