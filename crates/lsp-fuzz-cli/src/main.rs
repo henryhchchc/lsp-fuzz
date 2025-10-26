49a30be8fbc57b4a73d2b7b207b428fe3c12f5aa
Henry Chu <henrytech@outlook.com>
2025-10-26 15:25:03 +0800

    Split debug build warning into separate eprintln calls

--- crates/lsp-fuzz-cli/src/main.rs
+++ crates/lsp-fuzz-cli/src/main.rs
@@ -10,9 +10,8 @@ pub const PROGRAM_NAME: &str = "LSP-Fuzz";
 
 fn main() -> anyhow::Result<()> {
     if cfg!(debug_assertions) {
-        eprintln!(
-            "This is a debug build of {PROGRAM_NAME}.\nPlease use a release build for better performance."
-        )
+        eprintln!("This is a debug build of {PROGRAM_NAME}.");
+        eprintln!("Please use a release build for better performance.");
     }
 
     let cli = cli::Cli::parse();
